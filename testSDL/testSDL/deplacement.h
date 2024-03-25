75dfc2fc0a40f249657543c59357196ae86732cb
FTGabriel <94760998+FTGabriel@users.noreply.github.com>
2024-03-25 12:55:55 +0100

    Add files via upload

---
+++ testSDL/testSDL/deplacement.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "main.h";
+#include "initialisation.h"
+
+void vaisseau1();
+void vaisseau2();
---
+++ testSDL/testSDL/initialisation.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "main.h"
+#include <stdbool.h>
+
+SDL_Window* wind;
+SDL_Renderer* render;
+int hauteur;
+int epaisseur;
+int x1;
+int x2;
+int yvaisseau1;
+int yvaisseau2;
