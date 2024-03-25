9d47ad93e430b359bfec28cb1afdd8ae0da93e9a
ObsiNoa <151033096+ObsiNoa@users.noreply.github.com>
2024-03-25 13:01:43 +0100

    Add files via upload

---
+++ testSDL/main.c
@@ -0,0 +1,30 @@
+﻿#include <SDL.h>
+#include <stdio.h>
+#include "initialisation.h"
+
+int main(int argc, char* argv[]) {
+
+    init();
+    window();
+    renderer();
+
+
+    SDL_Rect dest_rect = { 10, 10, 640, 400 };
+    SDL_Surface* image = IMG_Load("C:/Users/nducrot/python/jeu/image/bouton_6.png");
+    if (!image)
+    {
+        printf("Erreur de chargement de l'image : %s", SDL_GetError());
+        return -1;
+    }
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(render, image);
+    SDL_RenderCopy(render, texture, NULL, NULL);
+
+    // Attendre quelques secondes avant de fermer la fenêtre
+    SDL_Delay(5000);
+
+    // Fermeture de la fenêtre et de la SDL
+    SDL_DestroyWindow(wind);
+    SDL_Quit();
+
+    return 0;
+}
---
+++ testSDL/main.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+#include <SDL.h>
+#include "initialisation.h"
+#include <SDL_image.h>
+
+void init();
+void window();
+void renderer();
